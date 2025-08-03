ff91dd8bd64d44598f2951f50c19256b57b52732
Priyesh <kpriyesh1908@gmail.com>
2025-08-04 00:58:46 +0530

    Add static web assets configuration for IndiaPortalClone project
    - Created staticwebassets.build.json to manage asset references and configurations.
    - Added staticwebassets.build.json.cache for caching purposes.
    - Introduced staticwebassets.development.json for development environment asset management.
    - Included compressed versions of CSS and JS files for optimized loading.
    - Defined asset properties such as integrity and fingerprint for security and cache management.

--- obj/Release/net9.0/IndiaPortalClone.AssemblyInfo.cs
+++ obj/Release/net9.0/IndiaPortalClone.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaPortalClone")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654f8e6670369d0f8154c92e1c21669fa292d825")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaPortalClone")]
