654f8e6670369d0f8154c92e1c21669fa292d825
Priyesh <kpriyesh1908@gmail.com>
2025-08-04 00:56:23 +0530

    Update project files and binaries for IndiaPortalClone
    - Updated AssemblyInfoInputs.cache with new hash.
    - Added appsettings.Production.json to the project file list.
    - Updated IndiaPortalClone.dll and IndiaPortalClone.pdb binaries.
    - Added sourcelink.json for source linking.
    - Updated rjsmcshtml.dswa.cache.json and rjsmrazor.dswa.cache.json with new asset fingerprints and metadata.
    - Updated rpswa.dswa.cache.json with new asset details and fingerprints.

--- obj/Debug/net9.0/IndiaPortalClone.AssemblyInfo.cs
+++ obj/Debug/net9.0/IndiaPortalClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaPortalClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd162870402904ef8183d6f723e126b67bad69e")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaPortalClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaPortalClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
